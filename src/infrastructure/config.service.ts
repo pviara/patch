795134597f83689083ebc241912f638c64203ee8
Pierre Viara <pierre.viara@outlook.com>
2022-07-22 22:30:38 +0200

    chore(setup): add linter and config service

---
+++ src/infrastructure/config.service.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+export class ConfigService {
+    static get appId(): string {
+        return this.getFromEnvFile('APP_ID');
+    }
+
+    static get guildId(): string {
+        return this.getFromEnvFile('GUILD_ID');
+    }
+
+    static get botToken(): string {
+        return this.getFromEnvFile('BOT_TOKEN');
+    }
+    
+    private static getFromEnvFile(key: string): string {
+        const value = process.env[key];
+        if (!value) {
+            throw new Error(`Could not retrieve "${key}" from env file`);
+        }
+
+        return value;
+    }
+}
